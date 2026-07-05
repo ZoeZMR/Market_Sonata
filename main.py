557d061ee4861969451d877a3332cd3081b487c1
Zoe Zhao <zoe_zhao2022@hotmail.com>
2026-07-05 18:59:17 -0400

    modify

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ def get_stock_price(ticker="AAPL"):
     print(f"\nLast 5 days of {ticker}:\n")
     print(history[["Open", "High", "Low", "Close", "Volume"]])
 
+
 if __name__ == "__main__":
     get_stock_price("AAPL")
